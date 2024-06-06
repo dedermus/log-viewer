c990a164c5960dc15a5fc4d137cd11c1c6a98390
dedermus <mapple@inbox.ru>
2024-06-06 23:19:42 +0300

    fix path to controller

--- resources/views/logs.blade.php
+++ resources/views/logs.blade.php
@@ -32,7 +32,7 @@
 
                         @foreach($logs as $index => $log)
                             <tr>
-                                <td><span class="badge bg-{{\OpenAdmin\Admin\LogViewer\LogViewer::$levelColors[$log['level']]}}">{{ $log['level'] }}</span></td>
+                                <td><span class="badge bg-{{\OpenAdminCore\Admin\LogViewer\LogViewer::$levelColors[$log['level']]}}">{{ $log['level'] }}</span></td>
                                 <td><strong>{{ $log['env'] }}</strong></td>
                                 <td style="width:150px;">{{ $log['time'] }}</td>
                                 <td><code style="word-break: break-all;">{{ $log['info'] }}</code></td>
